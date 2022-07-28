aa7ea1d6fce2cae851dd0dc7c11175296202fae1
azuolas <azuolasbalbieris@gmail.com>
2022-07-29 00:34:57 +0300

    Sum changes ;)

--- util.py
+++ util.py
@@ -4,12 +4,12 @@ def calculate_profit(buy_price: float, current_price: float, starting_sum: float
     return round((current_price/buy_price)*starting_sum-starting_sum, 2)
 
 def read_local_stocks_json():
-    stocks_json_file_readable = open("/home/$USER/stonks-cli/stocks.json", "r")
+    stocks_json_file_readable = open("stocks.json", "r")
     stocks_json = json.load(stocks_json_file_readable)
     return stocks_json
 
 def write_local_stocks_json(stocks_json):
-    with open('/home/$USER/stonks-cli/stocks.json', 'w', encoding='utf-8') as file:
+    with open('stocks.json', 'w', encoding='utf-8') as file:
         json.dump(stocks_json, file, ensure_ascii=False, indent=4)
 
 def check_if_empty(stocks_json):
